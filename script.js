2cd854c4bb2beb1245c0f5ed274c3aff23e003a8
Abednego Kibitok <abedysongol@gmail.com>
2023-05-04 13:40:01 +0300

    Modify script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const btn = document.querySelector('#navv');
 const bgr = document.querySelector('#headline');
+const dspl = document.querySelector('#none');
+const dspls = document.querySelector('.bgc');
 btn.addEventListener('click', () => {
-    bgr.setAttribute(
-        'style',
-         'background: #6070FF; border-radius: 0; mix-blend-mode: multiply; filter: blur(8px);'
-        );
+    bgr.setAttribute('style', 'background: #6070FF; border-radius: 0; mix-blend-mode: multiply; filter: blur(8px);');
+    dspl.setAttribute('style', 'display: none;');
+    dspls.setAttribute('style', 'display: none;');
 });
