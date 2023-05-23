6f2aa189676377a5645d0370aec775b74f8b3a58
Abednego Kibitok <abedysongol@gmail.com>
2023-05-23 20:47:29 +0300

    Modify script.js Modify index.html

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const headline = document.querySelector('#headline');
 const myProjects = document.querySelector('.works');
 const contactForm = document.querySelector('.bgc');
 const navLinks = document.querySelector('.navlinks');
-const myBlur = document.querySelector('#headlin');
+const myBlur = document.querySelector('.scti');
 const closeBtn = document.querySelector('#closebtn');
+const navLinks1 = document.querySelector('.navlinks1');
+const navLinks2 = document.querySelector('.navlinks2');
+const navLinks3 = document.querySelector('.navlinks3');
+const heading = document.querySelector('.heading');
 
 btn.addEventListener('click', () => {
   headline.setAttribute('style', 'background: #6070FF; border-radius: 0;');
@@ -14,6 +18,7 @@ btn.addEventListener('click', () => {
   myBlur.setAttribute('style', 'filter: blur(8px);');
   btn.setAttribute('style', 'display: none;');
   closeBtn.setAttribute('style', 'display: block;');
+  heading.setAttribute('style', 'visibility: hidden;');
 });
 
 closeBtn.addEventListener('click', () => {
@@ -25,4 +30,40 @@ closeBtn.addEventListener('click', () => {
   navLinks.setAttribute('style', 'display: none;');
   btn.setAttribute('style', 'display: block;');
   closeBtn.setAttribute('style', 'display: none;');
+});
+
+navLinks1.addEventListener('click', () => {
+  headline.setAttribute('style', 'border-radius: 0 0 0 100px / 100px;');
+  myProjects.setAttribute('style', 'display: block;');
+  contactForm.setAttribute('style', 'display: block;');
+  navLinks.setAttribute('style', 'display: block;');
+  myBlur.setAttribute('style', 'filter: 0;');
+  navLinks.setAttribute('style', 'display: none;');
+  btn.setAttribute('style', 'display: block;');
+  closeBtn.setAttribute('style', 'display: none;');
+  heading.setAttribute('style', 'visibility: visible;');
+});
+
+navLinks2.addEventListener('click', () => {
+  headline.setAttribute('style', 'border-radius: 0 0 0 100px / 100px;');
+  myProjects.setAttribute('style', 'display: block;');
+  contactForm.setAttribute('style', 'display: block;');
+  navLinks.setAttribute('style', 'display: block;');
+  myBlur.setAttribute('style', 'filter: 0;');
+  navLinks.setAttribute('style', 'display: none;');
+  btn.setAttribute('style', 'display: block;');
+  closeBtn.setAttribute('style', 'display: none;');
+  heading.setAttribute('style', 'visibility: visible;');
+});
+
+navLinks3.addEventListener('click', () => {
+  headline.setAttribute('style', 'border-radius: 0 0 0 100px / 100px;');
+  myProjects.setAttribute('style', 'display: block;');
+  contactForm.setAttribute('style', 'display: block;');
+  navLinks.setAttribute('style', 'display: block;');
+  myBlur.setAttribute('style', 'filter: 0;');
+  navLinks.setAttribute('style', 'display: none;');
+  btn.setAttribute('style', 'display: block;');
+  closeBtn.setAttribute('style', 'display: none;');
+  heading.setAttribute('style', 'visibility: visible;');
 });
